fb5092a0450b6f06c333db7b7cf153581937b67a
kytjy <kyliesophia.tan@gmail.com>
2024-03-16 01:56:11 +0800

    Commit skeleton for model building module

--- app.R
+++ app.R
@@ -11,7 +11,7 @@
 #~~~~~ Importing Packages~~~~~#
 #==============================#
 
-pacman::p_load("shiny", "fresh", "shinydashboard", "shinyWidgets", "shinythemes",
+pacman::p_load("shiny", "fresh", "shinydashboard", "shinydashboardPlus", "shinyWidgets", "shinythemes", "shinyjs",
                "tidyverse",
                "DT",
                "ExPanDaR", "kableExtra","plotly", "scales")
@@ -20,17 +20,18 @@ pacman::p_load("shiny", "fresh", "shinydashboard", "shinyWidgets", "shinythemes"
 #~~~~~ Data Manipulation ~~~~~#
 #==============================#
 
+# Loading files
 stu <- read_csv("data/stu_SG_rcd.csv")
 varlist <- read_csv("data/var.csv")
 
-
+# Dashboard - Summary Statistics
 Math <- round(summary(stu$Math),1)
 Reading <- round(summary(stu$Reading),1)
 Science <- round(summary(stu$Science),1)
 
 db_summarystats <- as.data.frame(rbind(Math, Reading, Science))
 
-# Dashboard Ternary Plot Tooltips
+# Dashboard - Ternary Plot Tooltips
 stu_tt <- stu %>% 
   mutate(rank_Math = round(percent_rank(Math)*100, 0),
          rank_Reading = round(percent_rank(Reading)*100, 0),
@@ -38,6 +39,10 @@ stu_tt <- stu %>%
          tooltip = paste0("Math: ", round(Math,0), " | Percentile: ", rank_Math,
                           "\nReading: ", round(Reading,0), " | Percentile: ", rank_Reading,
                           "\nScience: ", round(Science),  " | Percentile: ", rank_Science))
+
+# Regression Model
+stu_mb <- stu %>% 
+  select(2:26)
 
 #========================#
 #~~~~~ Theme ~~~~~#
@@ -52,10 +57,11 @@ mytheme <- create_theme(
     green = "#b1bdb5",
     yellow = "#d6ac5e",
     blue = "#4E7880",
+    aqua = "#4E7880",
     red = "#b3907A"
   ),
   adminlte_sidebar(
-    width = "180px",
+    width = "200px",
     dark_bg = "#f5f5eb",
     dark_hover_bg = "#DDAFA1",
     dark_color = "#f5f5eb",
@@ -92,15 +98,16 @@ header <- dashboardHeader(
   tags$li(class = "dropdown",
           tags$style(".main-header {max-height: 100px}"),
           tags$style(".main-header .logo {height: 100px; margin-top: 1em; margin-bottom: 1em}"),
-          tags$style(".sidebar-toggle {height: 100px;margin-top: 1em; margin-bottom: 1em; padding-top:1em; padding-botton:em;}")
+          tags$style(".sidebar-toggle {height: 100px;margin-top: 1em; margin-bottom: 1em;}")
   ),
   title = div(pplogo,
               style = "position: relative; margin:-3px 0px 0px 5px; display:right-align;"),
-  titleWidth = 100)
+  titleWidth = 80)
 
 ## Dashboard Sidebar ----------------------------------------------------
 sidebar <- dashboardSidebar(
-  tags$style(".left-side, .main-sidebar {padding-top: 120px}"),
+  tags$style(".left-side, .main-sidebar {padding-top: 150px; margin-left: 0px;}"),
+  width = 80,
   sidebarMenu(
     id = "tabs",
     menuItem("Home", tabName = "tab_home", icon = icon("house")),
@@ -115,7 +122,7 @@ sidebar <- dashboardSidebar(
              menuSubItem("Parallel Plot", tabName = "tab_parallelplot")
              ),
     
-    menuItem("Regression Models", tabName = "tab_mod", icon = icon("tree"),
+    menuItem("Regression", tabName = "tab_mod", icon = icon("tree"),
              menuSubItem("Decision Tree", tabName = "tab_dt"),             
              menuSubItem("Random Forest", tabName = "tab_rf"),
              menuSubItem("Gradient Boosting", tabName = "tab_gb")
@@ -133,8 +140,16 @@ body <- dashboardBody(
   
   ## CSS style  ----------------------------------------------------
   ## Header  ----------------------------------------------------
-  tags$head(tags$style(HTML('
-
+  tags$head(
+    tags$style(HTML('
+        
+        h1 {
+        
+        }
+        '))),
+        
+  tags$head(
+    tags$style(HTML('                
         /* logo */
         .skin-blue .main-header .logo {
                               background-color: #f5f5eb;
@@ -174,8 +189,8 @@ body <- dashboardBody(
         
         /* The toggle lines to collapse menu bar   */                 
          .skin-blue .main-header .navbar .sidebar-toggle:hover{
-                              color: #4E7880;
-                              background-color: #394434;
+                              color: #DDAFA1;
+                              background-color: #f5f5eb;
          }
         .navbar-custom-menu {
                               position: absolute;
@@ -183,8 +198,12 @@ body <- dashboardBody(
                               margin-top: 5px;
         }
         .navbar-custom-menu li {
-                                margin-left: 15px;
-                                }
+                                margin-top: 5px;
+                                margin-left: 10px;
+        }
+        
+        .sidebar-toggle {
+        }
         
          .myClass { 
               font-size: 200%;
@@ -192,7 +211,7 @@ body <- dashboardBody(
               text-align: left;
               font-family: "Playfair Display", serif;
               font-weight: bold;
-              padding: 0 10px;
+              padding: 0px;
               overflow: hidden;
               color: white;
             }
@@ -213,17 +232,16 @@ body <- dashboardBody(
   tabItems(
     ### Dashboard  ----------------------------------------------------
     tabItem(tabName = "tab_home",
-            fluidRow(
+            #fluidRow(
               
-              #### Dashboard First Column  ----------------------------------------------------
+              #### Dashboard Left Column  ----------------------------------------------------
               
-              column(width = 6,
-                     div(style = "padding = 0em; margin-left: 2em; margin-top: 2em; height: 100% ",
+              column(width = 5,
+                     div(style = "padding = 0em; margin-left: 0em; margin-top: 3em; height: 100% ",
                          fluidRow(
                            box(
                              title = "Welcome to Project PISA", #tags$p("", style = "color: #b3907A; font-weight: bold; font-size: 80%;"),
                             status = "primary",
-                            #solidHeader = TRUE,
                             collapsible = FALSE,
                             width = 12,
                             tags$div("According to the latest OECD's Programme for International Student Assessment (PISA) 2022, which measures 15-year-olds' ability to use their reading, mathematics, and science knowledge and skills to meet real-life challenges, socioeconomic status accounted for 17% of the variation in mathematics performance in Singapore (compared to 15% on average across OECD countries).",
@@ -237,7 +255,7 @@ body <- dashboardBody(
                            box(
                              title = tags$p("Relationship between subject scores", style = "font-weight: bold;"),
                              plotlyOutput("db_ternplot_",
-                                          height = "44vh",
+                                          height = "40vh",
                                           width = "100%"),
                              status = "primary",
                              width = 12
@@ -248,11 +266,11 @@ body <- dashboardBody(
 
                      ),
             
-            #### Dashboard Second Column  ----------------------------------------------------
+            #### Dashboard Right Column  ----------------------------------------------------
             column(width = 6,
                    
                    #### Dashboard Survey Stats Value Boxes  ----------------------------------------------------
-                   div(style = "padding = 0em; margin-left: 0em; margin-top: 3em; height: 100% ",
+                   div(style = "padding = 0em; margin-left: 1em; margin-top: 3em; height: 100% ",
                        fluidRow(
                           valueBoxOutput("home_studnum_", width = 6),
                           valueBoxOutput("home_schnum_", width = 6)
@@ -281,21 +299,162 @@ body <- dashboardBody(
                            )
                        )
                    )
-                )
+                #)
             ),
     
-    ### Dashboard  ----------------------------------------------------
+    ### Decision Tree  ----------------------------------------------------
     tabItem(tabName = "tab_dt",
             fluidRow(
-              column(width = 3,
-                     div(style = "padding = 0em; margin-right: -0.5em",
-                         box(title = tags$p("Variable Selection", style = "color: #FFF; font-weight: bold; font-size: 80%;"),
-                             status = "primary")
-                         )
+              column(width = 2,
+                     div(style = "padding = 0em; margin-left: 0em; margin-top: 3em; height: 100% ",
+                         box(title = tags$p("Variable Selection", style = "font-weight: bold;"),
+                             status = "primary",
+                             collapsible = FALSE,
+                             width = 12,
+                             div(style = "padding = 0em; margin-top: -0.5em",
+                                 awesomeRadio(
+                                   inputId = "mb_target_",
+                                   label = "Response Variable",
+                                   choices = c("Math", "Reading", "Science"),
+                                   selected = "Math",
+                                   status = "primary"
+                                 )),
+                             div(style = "padding = 0em; margin-top: -0.5em",
+                                 pickerInput(
+                                   inputId = "mb_vars_",
+                                   label = "Explanatory Variables",
+                                   choices = list(
+                                     `School Environment` = list("School Type" = "SchoolType",
+                                                                 "Loneliness" = "Loneliness",
+                                                                 "Classroom Safety" = "ClassroomSafety",
+                                                                 "Teacher Support" = "TeacherSupport"),
+                                     `Personal` = list("Gender" = "Gender",
+                                                       "Math Homework Time" = "Homework_Math",
+                                                       "Reading Homework Time" = "Homework_Reading",
+                                                       "Science Homework Time" = "Homework_Science",
+                                                       "Preference for Math" = "Preference_Math",
+                                                       "Preference for Reading" = "Preference_Reading",
+                                                       "Preference for Science" = "Preference_Science",
+                                                       "Exercise" = "Exercise"),
+                                     `Socioeconomic` = list("Parents' Education" = "ParentsEducation",
+                                                            "Immigration" = "Immigration",
+                                                            "Home Language" = "HomeLanguage",
+                                                            "Sibling" = "Sibling",
+                                                            "Aircon" = "Aircon",
+                                                            "Helper" = "Helper",
+                                                            "Vehicle" = "Vehicle",
+                                                            "Books" = "Books",
+                                                            "Own Room" = "OwnRoom",
+                                                            "Family Commitment" = "FamilyCommitment")),
+                                   selected = colnames(stu_mb[1:24]),
+                                   multiple = TRUE,
+                                   options = list(`actions-box` = TRUE),
+                                   choicesOpt = NULL,
+                                   inline = FALSE,
+                                   width = NULL
+                                 ))
+                             )
+                         ),
+                     ),
+              column(width = 2,
+                     div(
+                       style = "padding = 0em; margin-left: 0em; margin-top: 3em; height: 100% ",
+                       box(title = tags$p(span(icon("tree"), "Decision Tree"), style = "font-weight: bold;"),
+                           closable = FALSE,
+                           width = 12,
+                           status = "primary",
+                           solidHeader = FALSE,
+                           div(style = "padding = 0em; margin-top: -0.5em",
+                               chooseSliderSkin("Flat"),
+                               sliderInput(inputId = "dt_partition_",
+                                           label = "Train-Test Partition:",
+                                           min = 0.05,
+                                           max = 0.95,
+                                           value = c(0.8))),
+                           div(style = "padding = 0em; margin-top: -0.5em",
+                               tags$p("Select proportion of data used to train model", style = "font-style: italic;")),
+                           div(style = "padding = 0em; margin-top: 0.5em",
+                               numericInput(inputId = "dt_tunelength_",
+                                            label = "Initial Tune Length:",
+                                            value = 4,
+                                            min = 1,
+                                            max = 10,
+                                            step = 1
+                                            )),
+                           div(style = "padding = 0em; margin-top: 0.5em",
+                               selectInput(inputId = "dt_resamplingmethod_",
+                                           label = "Resampling Method:",
+                                           choices = c("Bootstrap" = "dt_bootstrap",
+                                                       "Cross Validation" = "dt_cvkfold",
+                                                       "Repeated Cross-Validation" = "dt_repeatkfold"),
+                                           selected = "dt_bootstrap"
+                                           )),
+                           hidden(div(style = "padding = 0em; margin-top: 0.5em",
+                                      numericInput(inputId = "dt_cvkfold_number",
+                                                   label = "K-fold:",
+                                                   min = 3,
+                                                   max = 50,
+                                                   step = 1,
+                                                   value = 10))),
+                           hidden(div(style = "padding = 0em; margin-top: 0.5em",
+                                      numericInput(inputId = "dt_repeatkfold_number",
+                                                   label = "K-fold:",
+                                                   min = 3,
+                                                   max = 50,
+                                                   step = 1,
+                                                   value = 10))),  
+                           hidden(div(style = "padding = 0em; margin-top: 0.5em",
+                                      numericInput(inputId = "dt_repeatkfold_repeat", 
+                                                   label = "Number of Repeats:",
+                                                   min = 3,
+                                                   max = 10,
+                                                   step = 1,
+                                                   value = 3))), 
+                           div(style = "padding = 0em; margin-top: 2em",
+                               actionButton(inputId = "dt_action_",
+                                            label ="Run Analysis",
+                                            icon = icon("wrench")),
+                               align = "center"
+                           ),
+                           # Box Sidebar for Model Tuning Parameters
+                           sidebar = boxSidebar(
+                             id = "dt_tuneparameters",
+                             label = "Tuning Parameters",
+                             width = 100,
+                             numericInput(inputId = "dt_cp", 
+                                          label = "Complexity Parameter:",
+                                          min = 0.01,
+                                          max = 1,
+                                          step = 0.001,
+                                          value = 0.02),
+                             actionButton(inputId = "dt_tunemodel",
+                                          label = "Tune Model",
+                                          icon = icon("scissors"))
+                             )
+                       )
                      )
-            ))
+
+                     ),
+              column(width = 8,
+                     div(
+                       style = "padding = 0em; margin-left: 0em; margin-top: 3em; height: 100% ",
+                       tabBox(title = tags$p("Results", style = "font-weight: bold;"),
+                              id = "dt_resultstb",
+                              width= 12,
+                              side = "right",
+                              selected = "Fit Assessment",
+                              tabPanel("Best Tune"),
+                              tabPanel("Decision Tree"),
+                              tabPanel("Fit Assessment")) 
+                       )
+                     )
+              )
+                     
+                     
     )
   )
+  )
+
 
 
 # User Interface  ----------------------------------------------------
